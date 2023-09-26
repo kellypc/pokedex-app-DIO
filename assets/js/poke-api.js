91b7b36b45f27a2c6324780428f0bbe6f849043b
Kelly Cruz <kellycruz.ti@gmail.com>
2023-09-26 14:02:35 -0300

    Pokemon details

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,16 @@
 const pokeApi = {}
 
+pokeApi.getPokemonDetail = (pokemon) => {
+  return fetch(pokemon.url)
+      .then((response) => response.json())
+}
+
 pokeApi.getPokemons = function (offset = 0, limit = 10) {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-  return fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.error(error))
+  return fetch(url) //busca de pokemon
+    .then((response) => response.json()) //conversão da lista para json
+    .then((jsonBody) => jsonBody.results) //pega a lista de pokemons
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail)) //transforma a lista em uma nova lista de promessa de detralhes do pokemon
+    .then((detailRequests) => Promise.all(detailRequests)) //espera a lista ser resolvida
+    .then((pokemonsDetails) => pokemonsDetails)
 }
